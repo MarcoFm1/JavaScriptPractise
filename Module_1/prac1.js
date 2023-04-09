7c46ff2b4122db0847ca7d19331ec87a9b04dad7
Marco Fini Minue <marco.fini22@gmail.com>
2023-04-09 18:32:19 -0300

    ojito con el examen

---
+++ Module_1/prac1.js
@@ -0,0 +1,16 @@
+// let num = prompt("Pone un numero:");
+// num = parseInt(num);
+
+// if (num > 5){
+//      alert("Number is higher than five");
+//  }
+//  else if (num === 5){
+//     alert("Number is equal than five")
+//  }
+//  else{
+//      alert("Number is lower than five");
+//  }
+ 
+
+
+
