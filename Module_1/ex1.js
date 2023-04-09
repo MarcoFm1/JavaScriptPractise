3b694be147673cfd85b9e225425185b2efaf1e1d
Marco Fini Minue <marco.fini22@gmail.com>
2023-04-09 19:20:32 -0300

    despues lo sigo, ahora valorant

---
+++ Module_1/ex1.js
@@ -0,0 +1,9 @@
+let robert = prompt("Ingresa el monto que tiene Robert: ");
+let peter = prompt("Ingresa el monto que tiene Peter: ");
+let cofla = prompt("Ingresa el monto que tiene Cofla: ");
+
+
+if(robert <= heladoDeAgua){
+    alert(``)
+}
+
