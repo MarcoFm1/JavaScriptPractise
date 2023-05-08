cd71f8e9c63a7afce6093b1bca2e046c58862def
Marco Fini Minue <marco.fini22@gmail.com>
2023-05-08 20:29:11 -0300

    read,e

--- Module_2/label.js
+++ Module_2/label.js
@@ -1,2 +1,10 @@
-let person = ["", "", "", ""];
-let person2 = ["", "", person, ""];
+let persons = ["marco", "fini", "minue", "curso"];
+let persons2 = ["septimo", "año", persons, "c"];
+
+for (let person2 in persons2){
+    if (person2 == "2"){
+        for(let person of persons){
+            document.write(person + "<br>");
+        };
+    }    
+};
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JavaScriptPractise
+
+Diff between "while" and "do while":
+
+while: ask and execute
+
+do while: execute and later ask
